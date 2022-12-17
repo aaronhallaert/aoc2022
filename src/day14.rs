a3a27f6aca61382c03e55db23e9e54d2a239bae5
Aaron Hallaert <hallaertaaron@gmail.com>
2022-12-17 11:45:56 +0100

    Day14: part 2

--- src/day14.rs
+++ src/day14.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 #[derive(Eq, PartialEq, Hash, Debug, Clone)]
-struct Position(usize, usize);
+struct Position(isize, isize);
 
 impl Position {
     fn fall_down(&mut self) {
@@ -39,8 +39,17 @@ impl Position {
         self.1 = 0;
     }
 
-    fn is_in_range(&self, min_x: usize, max_x: usize) -> bool {
+    /// returns true if falling down, it reaches the floor
+    fn can_fall_regarding_floor(&self, floor: isize) -> bool {
+        self.1 + 1 < floor
+    }
+
+    fn is_in_range(&self, min_x: isize, max_x: isize) -> bool {
         min_x <= self.0 && max_x >= self.1
+    }
+
+    fn blocks_source(&self) -> bool {
+        self.0 == 500 && self.1 == 0
     }
 }
 
@@ -69,22 +78,22 @@ fn main() {
             match (point_a, point_b) {
                 (Position(a1, a2), Position(b1, b2)) if a1 == b1 && b2 > a2 => {
                     for n in *a2 as u64..(*b2 as u64 + 1) {
-                        occupied_positions.insert(Position(*a1, n as usize));
+                        occupied_positions.insert(Position(*a1, n as isize));
                     }
                 }
                 (Position(a1, a2), Position(b1, b2)) if a1 == b1 && b2 < a2 => {
                     for n in *b2 as u64..(*a2 as u64 + 1) {
-                        occupied_positions.insert(Position(*a1, n as usize));
+                        occupied_positions.insert(Position(*a1, n as isize));
                     }
                 }
                 (Position(a1, a2), Position(b1, b2)) if a2 == b2 && b1 > a1 => {
                     for n in *a1 as u64..(*b1 as u64 + 1) {
-                        occupied_positions.insert(Position(n as usize, *a2));
+                        occupied_positions.insert(Position(n as isize, *a2));
                     }
                 }
                 (Position(a1, a2), Position(b1, b2)) if a2 == b2 && b1 < a1 => {
                     for n in *b1 as u64..(*a1 as u64 + 1) {
-                        occupied_positions.insert(Position(n as usize, *a2));
+                        occupied_positions.insert(Position(n as isize, *a2));
                     }
                 }
                 _ => {
@@ -97,38 +106,54 @@ fn main() {
     // find min x and max x
     let max_x = occupied_positions.iter().map(|p| p.0).max().unwrap();
     let min_x = occupied_positions.iter().map(|p| p.0).min().unwrap();
+    let floor = occupied_positions.iter().map(|p| p.1).max().unwrap() + 2;
+    println!("Floor: {}", floor);
 
     // Sand is pouring from (500, 0)
     let mut falling_sand = Position(500, 0);
     let mut sand_unit = 1;
     'new_sand: loop {
         'sand_fall: loop {
-            if !occupied_positions.contains(&falling_sand.peek_fall_down()) {
+            if !occupied_positions.contains(&falling_sand.peek_fall_down())
+                && falling_sand.can_fall_regarding_floor(floor)
+            {
                 falling_sand.fall_down();
-            } else if !occupied_positions.contains(&falling_sand.peek_fall_left()) {
+            } else if !occupied_positions.contains(&falling_sand.peek_fall_left())
+                && falling_sand.can_fall_regarding_floor(floor)
+            {
                 falling_sand.fall_left();
-            } else if !occupied_positions.contains(&falling_sand.peek_fall_right()) {
+            } else if !occupied_positions.contains(&falling_sand.peek_fall_right())
+                && falling_sand.can_fall_regarding_floor(floor)
+            {
                 falling_sand.fall_right();
             } else {
-                // sand stay put
+                // sand can't move
                 occupied_positions.insert(falling_sand.clone());
-
+                // stop falling
                 break 'sand_fall;
             }
 
             // check if the new position is still in range
-            if !falling_sand.is_in_range(min_x, max_x) {
-                sand_unit -= 1;
-                break 'new_sand;
-            };
+            // if !falling_sand.is_in_range(min_x, max_x) {
+            //     sand_unit -= 1;
+            //     break 'new_sand;
+            // };
         }
 
+        // PART 2
+        if falling_sand.blocks_source() {
+            // stop new_sand
+            break 'new_sand;
+        };
+
+
+        // println!("{}", sand_unit);
         sand_unit += 1;
         falling_sand.reset();
     }
 
-    println!("{:?}", occupied_positions);
+    // println!("{:?}", occupied_positions);
     println!("Min x: {:?}", min_x);
     println!("Max x: {:?}", max_x);
-    println!("Result part 1: {}", sand_unit);
+    println!("Result: {}", sand_unit);
 }
