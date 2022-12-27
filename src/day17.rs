b0b45db456fa6266a089f0686c80f77c88dfc896
Aaron Hallaert <hallaertaaron@gmail.com>
2022-12-27 17:15:17 +0100

    Day 17: part 1

--- src/day17.rs
+++ src/day17.rs
@@ -4,12 +4,11 @@ use std::{
     io::{self, BufRead},
 };
 
-#[derive(Hash, Eq, PartialEq, Clone)]
-struct Offset(usize, usize);
+const CAVE_WIDTH: usize = 7;
 
-#[derive(Clone)]
-struct BlockUnit {
-    mask: HashSet<Offset>,
+#[derive(Clone, Debug)]
+struct Block {
+    pub mask: Vec<(isize, isize)>,
 }
 
 enum BlockType {
@@ -20,64 +19,150 @@ enum BlockType {
     Cube,
 }
 
-impl BlockUnit {
+impl Block {
     fn new(block_type: BlockType) -> Self {
-        let mut h = HashSet::new();
         match block_type {
-            BlockType::HorizontalBar => {
-                h.insert(Offset(0, 0));
-                h.insert(Offset(1, 0));
-                h.insert(Offset(2, 0));
-                h.insert(Offset(3, 0));
-            }
-            BlockType::Cross => {
-                h.insert(Offset(0, 1));
-                h.insert(Offset(2, 1));
-                h.insert(Offset(1, 0));
-                h.insert(Offset(1, 1));
-                h.insert(Offset(1, 2));
-            }
-            BlockType::L => {
-                h.insert(Offset(0, 2));
-                h.insert(Offset(1, 2));
-                h.insert(Offset(2, 0));
-                h.insert(Offset(2, 1));
-                h.insert(Offset(2, 2));
-            }
-            BlockType::VerticalBar => {
-                h.insert(Offset(0, 0));
-                h.insert(Offset(0, 1));
-                h.insert(Offset(0, 2));
-                h.insert(Offset(0, 3));
-            }
-            BlockType::Cube => {
-                h.insert(Offset(0, 0));
-                h.insert(Offset(0, 1));
-                h.insert(Offset(1, 0));
-                h.insert(Offset(1, 1));
+            BlockType::HorizontalBar => Block {
+                mask: vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+            },
+            BlockType::Cross => Block {
+                mask: vec![(0, 1), (2, 1), (1, 0), (1, 1), (1, 2)],
+            },
+            BlockType::L => Block {
+                mask: vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+            },
+            BlockType::VerticalBar => Block {
+                mask: vec![(0, 0), (0, 1), (0, 2), (0, 3)],
+            },
+            BlockType::Cube => Block {
+                mask: vec![(0, 0), (0, 1), (1, 0), (1, 1)],
+            },
+        }
+    }
+
+    fn init_start(&mut self, cave: &Cave) {
+        let start_position = cave.calc_start_position();
+        self.mask.iter_mut().for_each(|offset| {
+            offset.0 += start_position.0;
+            offset.1 += start_position.1;
+        });
+    }
+
+    /// returns false if the movement did not occur
+    fn jet_push(&mut self, direction: &char, cave: &Cave) -> bool {
+        // check the spaces first
+        let can_move: bool = match direction {
+            '>' => self
+                .mask
+                .iter()
+                .map(|(a, b)| ((a + 1), *b))
+                .all(|space| cave.is_space_free(space)),
+            '<' => self
+                .mask
+                .iter()
+                .map(|(a, b)| ((a - 1), *b))
+                .all(|space| cave.is_space_free(space)),
+            _ => panic!("Parsing error"),
+        };
+
+        if can_move {
+            // update the block
+            match direction {
+                '>' => {
+                    self.mask.iter_mut().for_each(|space| {
+                        space.0 += 1;
+                    });
+                }
+                '<' => {
+                    self.mask.iter_mut().for_each(|space| {
+                        space.0 -= 1;
+                    });
+                }
+                _ => panic!("Parsing error"),
             }
         }
 
-        BlockUnit { mask: h }
-    }
-
-    fn init_start(&mut self, x: usize, y: usize) {
-        todo!()
+        can_move
     }
 
     /// returns false if the movement did not occur
-    fn jet_push(&mut self, direction: &char) -> bool {
-        match direction {
-            '>' => {}
-            '<' => {}
-            _ => panic!("Parsing error"),
-        };
-        todo!();
+    fn fall_down(&mut self, cave: &Cave) -> bool {
+        // check the spaces first
+        let can_move: bool = self
+            .mask
+            .iter()
+            .map(|(a, b)| (*a, b - 1))
+            .all(|space| cave.is_space_free(space));
+
+        if can_move {
+            self.mask.iter_mut().for_each(|space| space.1 -= 1)
+        }
+
+        can_move
+    }
+}
+
+struct Cave {
+    occupied_spaces: HashSet<(isize, isize)>,
+    dropped_blocks: usize,
+}
+
+impl Cave {
+    fn new() -> Self {
+        Self {
+            occupied_spaces: HashSet::new(),
+            dropped_blocks: 0,
+        }
     }
 
-    /// returns false if the movement did not occur
-    fn fall_down(&mut self) -> bool {
-        todo!();
+    fn print(&self, block: Option<&Block>) {
+        let max_height = self.occupied_spaces.iter().map(|s| s.1).max().unwrap_or(0) + 10;
+        println!("---------------------------");
+        println!("Max height: {}", max_height);
+        println!("Falling block: {:?}", block);
+
+        for y in (0..max_height + 1).rev() {
+            for x in 0..7 {
+                match self.occupied_spaces.contains(&(x, y)) {
+                    true => {
+                        print!("#")
+                    }
+                    false => {
+                        if let Some(block) = block {
+                            if block.mask.contains(&(x, y)) {
+                                print!("@")
+                            } else {
+                                print!(".")
+                            }
+                        } else {
+                            print!(".")
+                        }
+                    }
+                }
+            }
+            println!()
+        }
+    }
+
+    fn is_space_free(&self, space: (isize, isize)) -> bool {
+        !self.occupied_spaces.contains(&space)
+            && space.0 >= 0
+            && space.0 < CAVE_WIDTH as isize
+            && space.1 >= 0
+    }
+
+    fn calc_start_position(&self) -> (isize, isize) {
+        (
+            2,
+            self.occupied_spaces.iter().map(|s| s.1).max().unwrap_or(-1) + 4,
+        )
+    }
+
+    fn fix_block(&mut self, block: &Block) {
+        block.mask.iter().for_each(|offset| {
+            self.occupied_spaces.insert(*offset);
+        });
+        self.dropped_blocks += 1;
     }
 }
 
@@ -89,51 +174,63 @@ fn main() {
         .next()
         .unwrap()
         .unwrap()
-        .chars().collect::<Vec<char>>();
-    let mut movements = movements_vec.iter();
+        .chars()
+        .collect::<Vec<char>>();
+    let mut movements = movements_vec.iter().cycle();
 
+    let h_bar = Block::new(BlockType::HorizontalBar);
+    let v_bar = Block::new(BlockType::VerticalBar);
+    let l_block = Block::new(BlockType::L);
+    let cross = Block::new(BlockType::Cross);
+    let cube = Block::new(BlockType::Cube);
 
-    let h_bar = BlockUnit::new(BlockType::HorizontalBar);
-    let v_bar = BlockUnit::new(BlockType::VerticalBar);
-    let l_block = BlockUnit::new(BlockType::L);
-    let cross = BlockUnit::new(BlockType::Cross);
-    let cube = BlockUnit::new(BlockType::Cube);
-
-    let current_height = [0_usize; 7];
+    let mut cave = Cave::new();
 
     // loop till movements is empty
+
     'outer_loop: loop {
         for block in vec![
             h_bar.clone(),
-            v_bar.clone(),
-            l_block.clone(),
             cross.clone(),
+            l_block.clone(),
+            v_bar.clone(),
             cube.clone(),
         ]
         .iter_mut()
         {
-            block.init_start(2, current_height.iter().max().unwrap() + 3);
+            block.init_start(&cave);
+            // cave.print(Some(block));
 
             // loop till block is stuck
             'block_dropping: loop {
                 if let Some(direction) = movements.next() {
                     // jet push (take movement and figure out new position)
-                    block.jet_push(direction);
+                    block.jet_push(direction, &cave);
+                    // cave.print(Some(block));
 
-                    // fall 1 unit down
-                    let did_fall = block.fall_down();
+                    let did_fall = block.fall_down(&cave);
+                    // cave.print(Some(block));
 
                     if !did_fall {
+                        // update the cave
+                        cave.fix_block(block);
+                        if cave.dropped_blocks == 2022 {
+                            // cave.print(None);
+                            break 'outer_loop;
+                        };
                         break 'block_dropping;
-                    }
-                    else {
-                        // update the height vector
-                        todo!();
                     };
                 } else {
-                    break 'outer_loop;
+                    panic!("For some reason, cycle is broken");
                 }
             }
         }
     }
+
+    println!("Dropped blocks: {}", cave.dropped_blocks);
+
+    println!(
+        "Result: {}",
+        cave.occupied_spaces.iter().map(|s| s.1).max().unwrap() + 1
+    )
 }
